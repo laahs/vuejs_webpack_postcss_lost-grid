e160065743289db5b338d245bac2aed2c623ddaa
laahs <lkestaca@gmail.com>
2016-12-13 02:16:34 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+// require('./assets/css/stylesheet.css')
+require('./assets/css/stylesheet.css')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
